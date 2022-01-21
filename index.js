04472bf801a3c257f3e4c1b65e195cb61ec19fd6
Samantha Diaz <samidiaz3@gmail.com>
2022-01-21 12:20:34 -0500

    commit tree 513799e48423d33d88e716d5c563ec9a9c48d0d9

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution in this file!
+// let driver = new Object()
+const driver = {}
+
+// does not mutate original
+function updateDriverWithKeyAndValue(driver, key, value){
+    const newDriver = { ...driver }
+    newDriver[key] = value
+    return newDriver
+}
+
+// mutates the orignal
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+    driver[key] = value
+    return driver
+}
+
+// deletes; nondestructive
+function deleteFromDriverByKey(driver, key){
+    const newDriver = { ...driver }
+    delete newDriver[key]  // delete newDriver.key --> does not work 
+    return newDriver
+
+    // also works:
+        // const newDriver = Object.assign({}, driver)
+        // delete newDriver[key] 
+        // return newDriver
+}
+
+//  deletes; destructive
+function destructivelyDeleteFromDriverByKey(driver, key){
+    delete driver[key]
+    return driver
+}
+
+
